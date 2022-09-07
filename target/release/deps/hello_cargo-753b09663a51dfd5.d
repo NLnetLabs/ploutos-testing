ac2fc9926dbb8edb00c3fa709df44df6a91db010
ximon18 <3304436+ximon18@users.noreply.github.com>
2022-09-08 00:08:17 +0200

    Add missing config and data files.

---
+++ target/release/deps/hello_cargo-753b09663a51dfd5.d
@@ -0,0 +1,5 @@
+/home/ximon/src/org-reusable-workflow-testing/target/release/deps/hello_cargo-753b09663a51dfd5: src/main.rs
+
+/home/ximon/src/org-reusable-workflow-testing/target/release/deps/hello_cargo-753b09663a51dfd5.d: src/main.rs
+
+src/main.rs:
