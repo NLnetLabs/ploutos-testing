bae0193619c069d594947a166fa7ab6747f68a89
ximon18 <3304436+ximon18@users.noreply.github.com>
2022-10-26 22:56:48 +0200

    Add a systemd service.

--- pkg/test-scripts/test-mytest.sh
+++ pkg/test-scripts/test-mytest.sh
@@ -6,9 +6,13 @@ set -x
 case $1 in
   post-install)
     echo "POST INSTALL"
+    systemctl status mytest.timer
+    systemctl status mytest.service
     ;;
   post-upgrade)
     echo "POST UPGRADE"
+    systemctl status mytest.timer
+    systemctl status mytest.service
     ;;
   *)
     echo "OTHER: $1"
