b34a305109675e741edffad4dda16b8d77872f8c
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2022-10-27 00:01:44 +0200

    Is a sleep needed?

--- pkg/test-scripts/test-mytest.sh
+++ pkg/test-scripts/test-mytest.sh
@@ -6,11 +6,13 @@ set -x
 case $1 in
   post-install)
     echo "POST INSTALL"
+    sleep 70s
     systemctl status mytest.timer
     systemctl status mytest.service
     ;;
   post-upgrade)
     echo "POST UPGRADE"
+    sleep 70s
     systemctl status mytest.timer
     systemctl status mytest.service
     ;;
