8b525f03246ff46b25b1b4d491e83b0a86c2a66a
ximon18 <3304436+ximon18@users.noreply.github.com>
2022-10-27 10:29:18 +0200

    Don't exit with code 3 because systemctl status exits with that and pipefail causes us to experience that even with the pipe to grep.

--- pkg/test-scripts/test-mytest.sh
+++ pkg/test-scripts/test-mytest.sh
@@ -8,7 +8,7 @@ case $1 in
     sleep 5s # allow time for the timer to fire (fires every 2 seconds)
     systemctl status mytest.timer
     systemctl status mytest.service || true # returns exit code 3 if active but not currently running
-    systemctl status mytest.service | grep -F 'Hello, world!'
+    journalctl mytest.service | grep -F 'Hello, world!'
     ;;
   *)
     echo "OTHER: $1"
