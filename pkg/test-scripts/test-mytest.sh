bbe8299244c8be47c2fb98e014750ef5ece3dad7
ximon18 <3304436+ximon18@users.noreply.github.com>
2022-10-27 10:58:22 +0200

    Add missing flag to journalctl command.

--- pkg/test-scripts/test-mytest.sh
+++ pkg/test-scripts/test-mytest.sh
@@ -8,7 +8,7 @@ case $1 in
     sleep 5s # allow time for the timer to fire (fires every 2 seconds)
     systemctl status mytest.timer
     systemctl status mytest.service || true # returns exit code 3 if active but not currently running
-    journalctl mytest.service | grep -F 'Hello, world!'
+    journalctl --unit mytest.service | grep -F 'Hello, world!'
     ;;
   *)
     echo "OTHER: $1"
