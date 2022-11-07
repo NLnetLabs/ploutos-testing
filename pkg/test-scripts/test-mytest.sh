392d919825d7622e38bfc84abbf2346852b80e0d
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2022-11-07 21:56:58 +0100

    v3.0.1 (#21)
    Adds test data and test scripts to verify minimal and minimal-cross DEB profile selection has the intended effects.

--- pkg/test-scripts/test-mytest.sh
+++ pkg/test-scripts/test-mytest.sh
@@ -9,6 +9,24 @@ case $1 in
     systemctl status mytest.timer
     systemctl status mytest.service || true # returns exit code 3 if active but not currently running
     journalctl --unit mytest.service | grep -F 'Hello, world!'
+
+    # check that our package has the expected dependencies
+    ID=$(grep -E '^ID=' /etc/os-release | cut -d '=' -f 2 || true)
+    CODENAME=$(grep -E '^VERSION_CODENAME=' /etc/os-release | cut -d '=' -f 2 || true)
+
+    case $ID in
+      ubuntu|debian)
+        case $CODENAME in
+          xenial|bionic|stretch)
+            apt show mytest 2>&1 | grep -E '^Depends' | grep 'rsync'
+            ;;
+
+          *)
+            apt show mytest 2>&1 | grep -E '^Depends' | grep 'libssl'
+            ;;
+        esac
+        ;;
+    esac
     ;;
   *)
     echo "OTHER: $1"
